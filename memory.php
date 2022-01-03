9b0a0d1a4ca70da6e03b7b25745a16fa4459b7f0
Laura Savickaite <laura.savickaite@laplateforme.io>
2022-01-03 18:27:17 +0100

    création des images et de l'option shuffle :: début d'indice pour la session à voir cette piste et donc créer la base de données

--- memory.php
+++ memory.php
@@ -1,8 +1,18 @@
 <!-- 
-* plusieurs images sont présentées, il y a des pairs, elles sont arrangées sous forme de tableau 
-* leur ordre est randomnisé selon la partie == chaque nouvelle partie (une nouvelle session crée lors de l'appui d'un bouton "jouer") -> nouvel ordre.
-* quand le dos est cliqué -> l'image se retourne => face. La face n'est présentée que si et seulement si le dos est cliqué
- * deux clics possibles :: 
+I- plusieurs images sont présentées, une image est présentée deux fois, elles sont arrangées sous forme de tableau 
+    a) créer un tableau avec les images stockées
+    b) créer une boucle disant que chaque image doit être doublée
+    c) display ces images dans un tableau html // à voir
+
+II- leur ordre est randomnisé selon la partie == chaque nouvelle partie (une nouvelle session crée lors de l'appui d'un bouton "jouer") -> nouvel ordre.
+
+
+III- quand le dos est cliqué -> l'image se retourne => face. La face n'est présentée que si et seulement si le dos est cliqué
+    a)
+    b)
+    c)
+
+IV- deux clics possibles :: 
      -soit les deux images cliquées ne sont pas pareilles alors se retournent
      -soit les deux images cliquées sont pareilles alors ne se retournent plus 
 -->
@@ -12,13 +22,39 @@
 class Image {
     public $_face;
     public $_size;
+    public $_identifiant;
+    
+    public function __construct (string $face, int $identifiant){
+        $this->_face = $face;
+        $this->_identifiant = $identifiant;
+    }
 }
 
-function __construct ($face, $size){
-    $this -> _face = $face;
-    $this -> _size = $size;
+
+$lucifer = new Image ("Images/lucifer.png", 1);
+// var_dump($lucifer -> _face);
+$reading = new Image ("Images/reading.png", 2);
+$renaissance = new Image ("Images/renaissance.png", 3);
+$spring1 = new Image ("Images/spring1.png", 4);
+$spring2 = new Image ("Images/spring2.png", 5);
+$spring3 = new Image ("Images/spring3.png", 6);
+
+$faceUpArray = array($lucifer, $reading, $renaissance,$spring1, $spring2,
+$spring3);
+$faceUpArray = array_merge($faceUpArray,$faceUpArray);
+
+
+//ATTENTION QUAND JE CLIQUE SUR UNE CARTE IL ME REMET LES CARTE DANS L'ORDRE
+if(isset($_POST['startgame'])){
+    
+    // créer une session 
 }
 
+    // si une session existante
+    shuffle($faceUpArray); 
+    if (isset($_POST['face'])){
+        echo "ok";
+    }
 
 ?>
 
@@ -30,10 +66,25 @@ function __construct ($face, $size){
   <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <!-- <img src="Images/lucifer.png">
-    <img src="Images/lucifer.png">
-    <img src="Images/renaissance.png">
-    <img src="Images/renaissance.png"> -->
+
+    <form action="memory.php" method="post">
+        <button type="submit" name="startgame">Start game</button>
+    </form>
+
+
+    <form action="memory.php" method="post">
+
+    <?php 
+            foreach($faceUpArray as $faceUp) { 
+        ?> 
+                <button type="submit" name="face">
+                    <img src="<?php echo $faceUp->_face ?>">
+                </button>
+        <?php
+            }
+        ?>
+
+    </form>
 
 </body>
 </html>
