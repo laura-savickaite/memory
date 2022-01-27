7bc007ee2e3cf58f2fab50e12685ad2df8b248d9
Laura Savickaite <laura.savickaite@laplateforme.io>
2022-01-27 10:55:28 +0100

    landing page done

--- index.php
+++ index.php
@@ -4,9 +4,9 @@ require 'bdd_connect.php';
 
 session_start();
 
-if(isset($_SESSION)){
-    var_dump($_SESSION['user']);
-}
+// if(isset($_SESSION)){
+//     var_dump($_SESSION['user']);
+// }
 
 ?>
 
@@ -17,6 +17,9 @@ if(isset($_SESSION)){
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="style.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300&display=swap" rel="stylesheet">
     <title>Museum</title>
 </head>
 <body>
@@ -48,15 +51,30 @@ if(isset($_SESSION)){
                 <img src="Images/header3.jpeg" width="500px">  
             </div>
         </section>
-            <h1 class="titre-index">Museum of XXXXX</h1>
+            <h1 class="titre-index">Museum of Classical Paintings</h1>
         <section class="texte-index">
-            <p>The museum of XXX fête ses XXXX de son exposition XXXXX pour l'occasion venez jouer un petit jeu de memory pour XXXXXX</p>
+            <p>The museum of Classical Paintings celebrates its first anniversary of its Renaissance Art exposition. For this occasion, come play a little game of Memory and challenge the best!</p>
+            <button id="bouton-index" type="button">Let's play <span id="arrow">→</span></button>
+        </section>
+    </article>
+    <article class="museum-news">
+        <section class="row">
+            <div>lol</div>
+            <div>lol</div>
+        </section>
+        <section class="row">
+            <div>lol</div>
+            <div>lol</div>
+        </section>
+        <section class="row">
+            <div>lol</div>
+            <div>lol</div>
         </section>
     </article>
     </main>
 
     <footer>
-        <a href="#"><img src="Images/github.png" width="50px"></a>
+        <a href="#"><img src="Images/github.svg" width="50px"></a>
     </footer>
 </body>
 </html>
