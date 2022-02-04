5d89f178f7c8579d5bfd5d4ca0a165cc7aeb981a
Laura Savickaite <laura.savickaite@laplateforme.io>
2022-02-04 13:53:03 +0100

    changement de connexion bdd

--- bdd_connect.php
+++ bdd_connect.php
@@ -1,19 +1,19 @@
 <?php
-    $host='localhost';
-    $user='root';
-    $password='';
-    $dbname='memory';
-
-    $dsn='mysql:host='.$host.'; dbname='.$dbname.'; charset=utf8';
-
-    $pdo=new PDO($dsn, $user, $password);
-
     // $host='localhost';
-    // $user='laura-savickaite';
-    // $password='laurasavickaite1997';
-    // $dbname='laura-savickaite_memory';
+    // $user='root';
+    // $password='';
+    // $dbname='memory';
 
     // $dsn='mysql:host='.$host.'; dbname='.$dbname.'; charset=utf8';
 
     // $pdo=new PDO($dsn, $user, $password);
+
+    $host='localhost';
+    $user='laura-savickaite';
+    $password='laurasavickaite1997';
+    $dbname='laura-savickaite_memory';
+
+    $dsn='mysql:host='.$host.'; dbname='.$dbname.'; charset=utf8';
+
+    $pdo=new PDO($dsn, $user, $password);
 ?>
